7f3905a6667505720da612e7cadbc9c52f397eb1
macdonst <simon.macdonald@gmail.com>
2022-01-13 20:48:54 -0500

    Use http handler
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/http/get-todos/index.js
+++ src/http/get-todos/index.js
@@ -13,11 +13,8 @@ async function read() {
   await clientClose()
 
   return {
-    statusCode: 201,
-    headers: {
-      'content-type': 'application/json; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    body: JSON.stringify(todos)
+    statusCode: 200,
+    cacheControl: 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
+    json: todos
   }
 }
